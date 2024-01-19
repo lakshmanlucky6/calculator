dae3445b08d5c8060f812680b4ab86f6129d9500
lakshmanlucky6 <137479735+lakshmanlucky6@users.noreply.github.com>
2024-01-19 15:48:44 +0530

    calculator

---
+++ calc.js
@@ -0,0 +1,5 @@
+addTemp=function(x,y){ return x+y; };
+exports.add=addTemp;
+exports.sub=function(x,y){ return x-y; };
+exports.mult=function(x,y){ return x*y; };
+exports.div=function(x,y){ return x/y; };
---
+++ index.js
@@ -0,0 +1,6 @@
+var cal=require('./calc');
+var x=10,y=20;
+console.log("\nAddtion of "+x+" and "+y+" is "+cal.add(x,y));
+console.log("\nSubtraction of "+x+" and "+y+" is "+cal.sub(x,y));
+console.log("\nMultiplication of "+x+" and "+y+" is "+cal.mult(x,y));
+console.log("\nDivision of "+x+" and "+y+" is "+cal.div(x,y));
